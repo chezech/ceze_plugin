55a782b5cdbed98dd1b85e149063a15edeb8e510
chezech <chezech@gmail.com>
2016-08-18 23:14:45 -0400

    Register taxonomies

--- projectcharles_plugin_portfolio.php
+++ projectcharles_plugin_portfolio.php
@@ -40,9 +40,9 @@ function projectcharles_custom_post_setting() {
 /*#######################################################################################
 The portfolio post type taxonomy
 ##########################################################################################*/ 
-/*====================================================
-Register Custom Taxonomies
-======================================================*/
+
+//Register Custom Taxonomies
+
 
 add_action('init', 'projectcharles_identify_taxonomy');
 
@@ -71,74 +71,4 @@ function projectcharles_identify_taxonomy() {
                             )
                      );
 }
-/**#############################################################################
-TO create the data fields which will be filtered by the taxonomy.
-##########################################################################################*/ 
-add_filter("manage_edit-portfolio_columns", "sa");
-
-function sa($field){
-   $field = array(
-                    "cb" => "<input type='checkbox' />",
-                    "photo" => __("Image"),
-                    "title" => __("Portfolio"),
-                    "portfolio_category" => __("Portfolio Category"),
-                    "date" => __("Date")
-                   );
-
-   return $field;
-}
-
-add_action("manage_portfolio_posts_custom_column",  "da");
-
-function da($field){
-  global $post;
-  switch ($field){
-                 case "photo":
-                     if(has_post_thumbnail()) the_post_thumbnail(array(50,50));
-                 break;
-                 case "portfolio_category":
-                     echo get_the_term_list($post->ID, 'portfolio_category', '', ', ','');
-                 break;
-   }
-}
-
-if ( isset($_GET['post_type']) ) {
-   $post_type = $_GET['post_type'];
-}else {
-   $post_type = '';
-}
-
-if ( $post_type == 'portfolio' ) {
-   add_action( 'restrict_manage_posts','wutan' );
-   add_filter( 'parse_query','perform_filtering' );
-}
-
-function wutan() {
-   global $typenow, $wp_query;
-   if ($typenow=='portfolio') {
-      wp_dropdown_categories(array(
-                                   'show_option_all' => 'Show All Portfolio Category',
-                                   'taxonomy' => 'portfolio_category',
-                                   'name' => 'portfolio_category',
-                                   'orderby' => 'name',
-                                   'selected' =>( isset( $wp_query->query['portfolio_category'] ) ? $wp_query->query['portfolio_category'] : '' ),
-                                   'hierarchical' => false,
-                                   'depth' => 3,
-                                   'show_count' => false,
-                                   'hide_empty' => true,
-                            ));
-
-   }
-}
-
-function perform_filtering( $query ){
-   $qv = &$query->query_vars;
-   if (( $qv['portfolio_category'] ) && is_numeric( $qv['portfolio_category'] ) ) {
-      $term = get_term_by( 'id', $qv['portfolio_category'], 'portfolio_category' );
-      $qv['portfolio_category'] = $term->slug;
-   }
-}
-
-
-
 ?>
