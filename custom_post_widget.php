f493855f1cd280704dfac9e1e19bad41c25b34b4
chezech <chezech@gmail.com>
2016-08-18 22:45:42 -0400

    Plugin initialized

---
+++ custom_post_widget.php
@@ -0,0 +1,12 @@
+<?php
+/*
+Plugin Name: Projectcharles plugin
+Plugin URI: http://phoenix.sheridanc.on.ca/~ccit3679
+Description: This plugin adds a custom post type, a widget, and a shortcode to a portfolio site.
+Author: Charles Ezechukwu
+Version: 1.0
+Author URI: http://phoenix.sheridanc.on.ca/~ccit3679
+License: GPLv2
+*/
+
+?>
